b4d7d486c522f02c30908bbdf25e28e658280ecc
nightwing <amirjanyan@gmail.com>
2014-07-22 01:05:23 +0400

    tweak cssLint error handling

--- css_handler.js
+++ css_handler.js
@@ -18,28 +18,29 @@ handler.analyze = function(value, ast, callback) {
     callback(handler.analyzeSync(value, ast));
 };
 
+// 0: ignore, 1: warning, 2: error, 3: info
 var CSSLint_RULESET = {
     "adjoining-classes"           : 0,
     "box-model"                   : 1,
     "box-sizing"                  : 1,
-    "compatible-vendor-prefixes"  : 1,
+    "compatible-vendor-prefixes"  : 3,
     "display-property-grouping"   : 1,
     "duplicate-background-images" : 1,
     "duplicate-properties"        : 1,
     "empty-rules"                 : 1,
-    "errors"                      : 1,
-    "fallback-colors"             : 1,
+    "errors"                      : 2,
+    "fallback-colors"             : 3,
     "floats"                      : 1,
     "font-faces"                  : 1,
     "font-sizes"                  : 1,
-    "gradients"                   : 1,
-    // "ids"                      : 1,
-    "import"                      : 1,
-    "important"                   : 1,
+    "gradients"                   : 3,
+    "ids"                         : 0,
+    "import"                      : 0,
+    "important"                   : 3,
     "known-properties"            : 1,
-    "outline-none"                : 1,
+    "outline-none"                : 3,
     "overqualified-elements"      : 1,
-    "qualified-headings"          : 1,
+    "qualified-headings"          : 3,
     "regex-selectors"             : 1,
     "rules-count"                 : 1,
     "shorthand"                   : 1,
@@ -49,14 +50,9 @@ var CSSLint_RULESET = {
     "unique-headings"             : 1,
     "universal-selector"          : 1,
     "unqualified-attributes"      : 1,
-    "vendor-prefix"               : 1,
+    "vendor-prefix"               : 3,
     "zero-units"                  : 0
 };
-
-var INFO_MARKERS = [
-    "gradients",
-    "compatible-vendor-prefixes",
-]
 
 handler.analyzeSync = function(value, ast) {
     value = value.replace(/^(#!.*\n)/, "//$1");
@@ -65,7 +61,7 @@ handler.analyzeSync = function(value, ast) {
     var warnings = results ? results.messages : [];
 
     return warnings.map(function(warning) {
-        if (INFO_MARKERS.indexOf(warning.rule.id) > -1)
+        if (CSSLint_RULESET[warning.rule.id] === 3)
             warning.type = "info";
         return {
             pos: {
